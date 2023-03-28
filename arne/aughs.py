b6118266570225c04f7ba2cf86de87d8e9cc69fe
Arne Gevaert <Arne Gevaert>
2023-03-28 11:46:29 +0200

    optimizations in shrinkage

--- arne/aughs.py
+++ arne/aughs.py
@@ -47,63 +47,6 @@ def _update_tree_values(dt, value, node):
     # assert not np.isnan(dt.tree_.impurity[node]), "Impurity is NaN"
 
 
-def _shrink_tree_rec(dt, shrink_mode, lmb=0,
-                     X_train=None,
-                     X_train_parent=None,
-                     node=0, parent_node=None, parent_val=None, cum_sum=None):
-    """
-    Go through the tree and shrink contributions recursively
-    """
-    left = dt.tree_.children_left[node]
-    right = dt.tree_.children_right[node]
-    feature = dt.tree_.feature[node]
-    threshold = dt.tree_.threshold[node]
-    parent_num_samples = dt.tree_.n_node_samples[parent_node]
-    parent_feature = dt.tree_.feature[parent_node]
-    value = _normalize_value(dt, node)
-
-    # cum_sum contains the value of the telescopic sum
-    # If root: initialize cum_sum to the value of the root node
-    if parent_node is None:
-        cum_sum = value
-    else:
-        # If not root: update cum_sum based on the value of the current node and the parent node
-        reg = 1
-        if shrink_mode == "hs":
-            # Classic hierarchical shrinkage
-            reg = 1 + (lmb / parent_num_samples)
-        else:
-            parent_split_feature = X_train_parent[:, parent_feature]
-            if shrink_mode in ["hs_entropy", "hs_entropy_2"]:
-                # Note: we can just use the value_counts, scipy.stats.entropy
-                # handles normalization. i.e. it is not necessary to divide by
-                # the total number of samples
-                _, counts = np.unique(parent_split_feature, return_counts=True)
-                entropy = scipy.stats.entropy(counts)
-                if shrink_mode == "hs_entropy":
-                    # Entropy-based shrinkage
-                    reg = 1 + (lmb * entropy / parent_num_samples)
-                elif shrink_mode == "hs_entropy_2":
-                    # Entropy-based shrinkage, but entropy term is added
-                    # outside of the fraction
-                    reg = entropy * (1 + lmb / parent_num_samples)
-            elif shrink_mode == "hs_log_cardinality":
-                # Cardinality-based shrinkage
-                cardinality = len(np.unique(parent_split_feature))
-                reg = 1 + (lmb * np.log(cardinality) / parent_num_samples)
-        cum_sum += (value - parent_val) / reg
-
-    _update_tree_values(dt, cum_sum, node)
-    # If not leaf: recurse
-    if not (left == -1 and right == -1):
-        X_train_left = X_train[X_train[:, feature] <= threshold]
-        X_train_right = X_train[X_train[:, feature] > threshold]
-        _shrink_tree_rec(dt, shrink_mode, lmb, X_train_left, X_train, left,
-                            node, value, cum_sum.copy())
-        _shrink_tree_rec(dt, shrink_mode, lmb, X_train_right, X_train,
-                            right, node, value, cum_sum.copy())
-
-
 class ShrinkageEstimator(BaseEstimator):
     def __init__(self, base_estimator: BaseEstimator = None,
                  shrink_mode: str = "hs", lmb: float = 1,
@@ -117,6 +60,69 @@ class ShrinkageEstimator(BaseEstimator):
     def get_default_estimator(self):
         raise NotImplemented
     
+    def _compute_node_entropies(self, dt, X_train, node=0, entropies=None, cardinalities=None):
+        left = dt.tree_.children_left[node]
+        right = dt.tree_.children_right[node]
+        feature = dt.tree_.feature[node]
+
+        if entropies is None:
+            entropies = np.zeros(len(dt.tree_.n_node_samples))
+            cardinalities = np.zeros(len(dt.tree_.n_node_samples))
+
+        # If not leaf node, compute entropy and cardinality of the node
+        if not (left == -1 and right == -1):
+            split_feature = X_train[:, feature]
+            _, counts = np.unique(split_feature, return_counts=True)
+            entropies[node] = scipy.stats.entropy(counts)
+            cardinalities[node] = len(counts)
+
+            # Recursively compute entropy and cardinality of the children
+            self._compute_node_entropies(dt, X_train, left, entropies, cardinalities)
+            self._compute_node_entropies(dt, X_train, right, entropies, cardinalities)
+        return entropies, cardinalities
+
+    
+    def _shrink_tree_rec(self, dt, dt_idx, node=0, parent_node=None,
+                         parent_val=None, cum_sum=None):
+        """
+        Go through the tree and shrink contributions recursively
+        """
+        left = dt.tree_.children_left[node]
+        right = dt.tree_.children_right[node]
+        parent_num_samples = dt.tree_.n_node_samples[parent_node]
+        value = _normalize_value(dt, node)
+
+        # cum_sum contains the value of the telescopic sum
+        # If root: initialize cum_sum to the value of the root node
+        if parent_node is None:
+            cum_sum = value
+        else:
+            # If not root: update cum_sum based on the value of the current node and the parent node
+            reg = 1
+            if self.shrink_mode == "hs":
+                # Classic hierarchical shrinkage
+                reg = 1 + (self.lmb / parent_num_samples)
+            else:
+                if self.shrink_mode in ["hs_entropy", "hs_entropy_2"]:
+                    entropy = self.entropies_[dt_idx][parent_node]
+                    if self.shrink_mode == "hs_entropy":
+                        # Entropy-based shrinkage
+                        reg = 1 + (self.lmb * entropy / parent_num_samples)
+                    elif self.shrink_mode == "hs_entropy_2":
+                        # Entropy-based shrinkage, but entropy term is added
+                        # outside of the fraction
+                        reg = entropy * (1 + self.lmb / parent_num_samples)
+                elif self.shrink_mode == "hs_log_cardinality":
+                    # Cardinality-based shrinkage
+                    cardinality = self.cardinalities_[dt_idx][parent_node]
+                    reg = 1 + (self.lmb * np.log(cardinality) / parent_num_samples)
+            cum_sum += (value - parent_val) / reg
+        _update_tree_values(dt, cum_sum, node)
+        # If not leaf: recurse
+        if not (left == -1 and right == -1):
+            self._shrink_tree_rec(dt, dt_idx, left, node, value, cum_sum.copy())
+            self._shrink_tree_rec(dt, dt_idx, right, node, value, cum_sum.copy())
+
     def fit(self, X, y, **kwargs):
         X, y = self._validate_arguments(X, y, kwargs.pop("feature_names", None))
 
@@ -131,29 +137,43 @@ class ShrinkageEstimator(BaseEstimator):
         # Save a copy of the original estimator
         self.orig_estimator_ = deepcopy(self.estimator_)
 
-        # Apply hierarchical shrinkage
-        self.shrink(X)
-
-        return self
-    
-    def shrink(self, X):
+        # Compute node entropies
+        self.entropies_ = []
+        self.cardinalities_ = []
         if hasattr(self.estimator_, "estimators_"):  # Random Forest
             for estimator in self.estimator_.estimators_:
-                _shrink_tree_rec(estimator, self.shrink_mode, self.lmb, X)
+                entropies, cardinalities = self._compute_node_entropies(estimator, X)
+                self.entropies_.append(entropies)
+                self.cardinalities_.append(cardinalities)
         else:  # Single tree
-            _shrink_tree_rec(self.estimator_, self.shrink_mode, self.lmb, X)
+            entropies, cardinalities = self._compute_node_entropies(self.estimator_, X)
+            self.entropies_.append(entropies)
+            self.cardinalities_.append(cardinalities)
+
+        # Apply hierarchical shrinkage
+        self.shrink()
+        return self
+    
+    def shrink(self):
+        if hasattr(self.estimator_, "estimators_"):  # Random Forest
+            for i, estimator in enumerate(self.estimator_.estimators_):
+                self._shrink_tree_rec(estimator, i)
+        else:  # Single tree
+            self._shrink_tree_rec(self.estimator_, 0)
     
     def set_shrink_params(self, X, shrink_mode=None, lmb=None):
         if shrink_mode is not None:
             self.shrink_mode = shrink_mode
         if lmb is not None:
             self.lmb = lmb
+        if not hasattr(self, "entropies_"):
+            raise ValueError("Cannot set shrinkage parameters before fitting")
         
         # Reset the estimator to the original one
         self.estimator_ = deepcopy(self.orig_estimator_)
-
+        
         # Apply hierarchical shrinkage
-        self.shrink(X)
+        self.shrink()
 
     def _validate_arguments(self, X, y, feature_names):
         if self.shrink_mode not in ["hs", "hs_entropy", "hs_entropy_2",
@@ -194,7 +214,7 @@ class ShrinkageRegressor(ShrinkageEstimator, RegressorMixin):
 
 
 def cross_val_lmb(shrinkage_estimator, X, y, shrink_mode, lmb_range, n_splits,
-                  score_fn="balanced_accuracy"):
+                  score_fn="balanced_accuracy", n_jobs=-1):
     lmb_scores = []
     cv = KFold(n_splits=n_splits, shuffle=True)
 
@@ -215,9 +235,14 @@ def cross_val_lmb(shrinkage_estimator, X, y, shrink_mode, lmb_range, n_splits,
                 raise ValueError("Invalid score function")
         return scores
 
-    lmb_scores = np.array(Parallel(n_jobs=-1)(delayed(_single_fold)(
-        train_index, test_index, X, y, lmb_range, shrink_mode)
-        for train_index, test_index in cv.split(X)))
+    if n_jobs != 1:
+        lmb_scores = np.array(Parallel(n_jobs=-1)(delayed(_single_fold)(
+            train_index, test_index, X, y, lmb_range, shrink_mode)
+            for train_index, test_index in cv.split(X)))
+    else:
+        lmb_scores = np.array([_single_fold(
+            train_index, test_index, X, y, lmb_range, shrink_mode)
+            for train_index, test_index in cv.split(X)])
 
     return np.average(lmb_scores, axis=0)
 
